f2b0f4ef27639049c04a4771bd00ae9314fc5293
tanyasanovna <tanyasanovna@gmail.com>
2022-01-31 02:18:31 +0300

    Домашнее задание неделя 13

---
+++ HW13/HW13_2.js
@@ -0,0 +1,23 @@
+function sum(a, b) {
+    a = prompt("Введите первое число");
+    b =  prompt("Введите второе число");
+    alert(Number(a) + Number(b));
+}
+
+function sub(a, b) {
+    a = prompt("Введите первое число");
+    b =  prompt("Введите второе число");
+    alert(a - b);
+}
+
+function div(a, b) {
+    a = prompt("Введите первое число");
+    b =  prompt("Введите второе число");
+    alert(a / b);
+}
+
+function mult(a, b) {
+    a = prompt("Введите первое число");
+    b =  prompt("Введите второе число");
+    alert(a * b);
+}
